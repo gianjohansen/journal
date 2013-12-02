55edf9c3b5d41022b669450fd0ecf4846a8b5787
gianjohansen <gian.johansen@gmail.com>
2013-12-02 13:49:56 +1100

    Form submit is done via ajax

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,3 +7,17 @@ $('#open-contact-modal').click(function(e){
   $('#contact-modal').trigger('openModal');
   e.preventDefault();
 });
+
+$('#contact-form').submit(function(form) {
+$.ajax({
+          dataType: 'jsonp',
+          url: "http://getsimpleform.com/messages/ajax?form_api_token=eca2adb05b1ade6f67db49e9bdab0822",
+          data: $('#contact-form').serialize() 
+        }).done(function() {
+          //callback which can be used to show a thank you message
+          //and reset the form
+          alert("Thank you, for contacting us");
+        });
+        return false; //to stop the form from submitting
+
+});
