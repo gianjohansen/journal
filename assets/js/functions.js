861d74947d21a3058423ea922644176c141b0f98
gianjohansen <gian.johansen@gmail.com>
2013-12-02 20:21:03 +1100

    Archive list should collapse on small screens

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,27 +1,30 @@
-$('#contact-modal').easyModal({
-  overlayOpacity : 0.6,
-  overlayClose: true,
-  overlayColor: "#000"
-});
-$('#open-contact-modal').click(function(e){
-  $('#contact-modal').trigger('openModal');
-  e.preventDefault();
-});
-$('#contact-success-modal').easyModal({
-  overlayOpacity : 0.6,
-  overlayClose: true,
-  overlayColor: "#000"
-});
+// set up contact modals when page loads
+$( document ).ready(function() {
+	$('#contact-modal').easyModal({
+	  overlayOpacity : 0.6,
+	  overlayClose: true,
+	  overlayColor: "#000"
+	});
+	$('#open-contact-modal').click(function(e){
+	  $('#contact-modal').trigger('openModal');
+	  e.preventDefault();
+	});
+	$('#contact-success-modal').easyModal({
+	  overlayOpacity : 0.6,
+	  overlayClose: true,
+	  overlayColor: "#000"
+	});
 
-$('#contact-form').submit(function(form) {
-$.ajax({
-          dataType: 'jsonp',
-          url: "http://getsimpleform.com/messages/ajax?form_api_token=eca2adb05b1ade6f67db49e9bdab0822",
-          data: $('#contact-form').serialize() 
+	$('#contact-form').submit(function(form) {
+	$.ajax({
+        dataType: 'jsonp',
+        url: "http://getsimpleform.com/messages/ajax?form_api_token=eca2adb05b1ade6f67db49e9bdab0822",
+        data: $('#contact-form').serialize() 
         }).done(function() {
 			$('#contact-modal').trigger('closeModal');
 			$('#contact-success-modal').trigger('openModal');
         });
         return false; //to stop the form from submitting
 
+	});
 });
