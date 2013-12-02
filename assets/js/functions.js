8287c02f4e555fc73cb69f043d5553ff05ef277b
gianjohansen <gian.johansen@gmail.com>
2013-12-02 14:35:56 +1100

    Dismiss modal on form submit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -19,6 +19,7 @@ $.ajax({
           url: "http://getsimpleform.com/messages/ajax?form_api_token=eca2adb05b1ade6f67db49e9bdab0822",
           data: $('#contact-form').serialize() 
         }).done(function() {
+			$('#contact-modal').trigger('closeModal');
 			$('#contact-success-modal').trigger('openModal');
         });
         return false; //to stop the form from submitting
