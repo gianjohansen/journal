24c9b1d504a5d9492fae7f718ad23ad07922d4d6
gianjohansen <gian.johansen@gmail.com>
2013-12-02 13:38:54 +1100

    Adding SimpleForm

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,19 +7,3 @@ $('#open-contact-modal').click(function(e){
   $('#contact-modal').trigger('openModal');
   e.preventDefault();
 });
-
-$('#contact-form').submit(function(form) {
-	alert("submitting");
-	$(form).ajaxSubmit({
-		type:"POST",
-		data: $(form).serialize(),
-		url:"process.php",
-		success: function() {
-			alert("success");
-		},
-		error: function() {
-			alert("error");
-		}
-	});
-	return false;
-});
